53af186e1a212a73d108806776b6d96a08a99502
shiningz01 <31381684+shiningz01@users.noreply.github.com>
2017-08-27 16:30:26 +0700

    2-38

---
+++ 2-38.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("%1s\n","58224090133");
+	printf("%1s\n","Santiphong Lapprayot");
+	printf("%1s\n\n\n\n\n","21/06/25");
+	printf("%120s\n\n","http://www.npru.ac.th");
+	printf("%57s%s","","Bye Bye");
+	return 0;
+}
